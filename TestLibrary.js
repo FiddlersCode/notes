ed40e4e18503bf573caf736b09656ec84693baeb
milesillsley <milesillsley@hotmail.co.uk>
2017-05-22 16:15:44 +0100

    Add isFalsy and isEqual matchers to testing library

--- TestLibrary.js
+++ TestLibrary.js
@@ -4,7 +4,25 @@
       if (!assertion) {
         throw new Error("Assertion failed: " + assertion + " is not truthy");
       }
+    },
+    isFalse: function(assertion) {
+      if (!!assertion) {
+        throw new Error("Assertion failed: " + assertion + " is not falsey");
+      }
+    },
+    isEqual: function(a, b) {
+      if (a !== b) {
+        throw new Error("Assertion failed: " + a + " is not equal to " + b);
+      }
+    },
+    isNotEqual: function(a, b) {
+      if (a === b) {
+        throw new Error("Assertion failed: " + a + " is equal to " + b);
+      }
     }
   };
-  exports.assert = assert;    
+  exports.assert = assert;
 })(this);
+
+expect(countModel instanceof Countmodel).toBe(true)
+assert.instanceof(countModel,Countmodel)
