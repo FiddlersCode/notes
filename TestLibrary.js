c39c7eda08f1be780ed971f50f1908a976f3818d
Robert Jones <robert.g.jones@gmail.com>
2017-05-22 15:35:22 +0100

    Add simple html to use TestLibrary; write test for truthyness; create empty note-model file

---
+++ TestLibrary.js
@@ -0,0 +1,10 @@
+(function(exports) {
+  var assert = {
+    isTrue: function(assertion) {
+      if (!assertion) {
+        throw new Error("Assertion failed: " + assertion + " is not truthy");
+      }
+    }
+  };
+  exports.assert = assert;    
+})(this);
