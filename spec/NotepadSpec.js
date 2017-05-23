35419ac3a4b70c51088cd2ef0abeb0b716f8a289
Paula <pmuldoon@umich.edu>
2017-05-23 12:45:32 +0100

    working on .length test

--- spec/NotepadSpec.js
+++ spec/NotepadSpec.js
@@ -1,15 +1,16 @@
-(function() {
-  console.log('Unit test: NotePad can store and retrieve a note');
-  var notepad, note;
-  notepad = new NotePad();
-  note = new Note('positive');
+// (function() {
+//   console.log('Unit test: NotePad can store and retrieve a note');
+//   var notepad, note;
+//   notepad = new NotePad();
+//   note = new Note('positive');
+//
+//   assert.isConstructedFrom(notepad, NotePad);
+//   notepad.addNote(note);
+//   assert.isConstructedFrom(notepad.getNote(), Note);
+//   assert.isEqual(notepad.getNote().text(),'positive');
+//   console.log('Unit test passed');
+// })();
 
-  assert.isConstructedFrom(notepad, NotePad);
-  notepad.addNote(note);
-  assert.isConstructedFrom(notepad.getNote(), Note);
-  assert.isEqual(notepad.getNote().text(),'positive');
-  console.log('Unit test passed');
-})();
 (function() {
   console.log('Unit test: NotePad can store and retrieve several notes');
   var notepad, note;
@@ -17,10 +18,16 @@
   note1 = new Note('positive');
   note2 = new Note('very positive');
   note3 = new Note('ur ok');
+  console.log(note1.text());
+  console.log(note2.text());
+  console.log(note3.text());
+  console.log(notepad);
 
   notepad.addNote(note1);
   notepad.addNote(note2);
   notepad.addNote(note3);
+  console.log(notepad.getAllNotes());
+  assert.isEqual(notepad.length === 3);
   assert.isEqual(notepad.getAllNotes(),['positive','very positive','ur ok']);
   console.log('Unit test passed');
 })();
--- src/NotepadModel.js
+++ src/NotepadModel.js
@@ -1,12 +1,20 @@
 function NotePad() {
   this._notes = [];
+  this._allNotes = [];
 }
+
 NotePad.prototype.addNote = function(note) {
   this._notes.push(note);
 };
-NotePad.prototype.getNote = function() {
-  return this._notes[0];
-};
+
+// NotePad.prototype.getNote = function() {
+//   return this._notes[0];
+// };
+
 NotePad.prototype.getAllNotes = function() {
-  return this._notes;
+  this._notes.forEach(function (note) {
+    console.log(note);
+    note.text();
+    this._allNotes.push(note);
+  });
 };
