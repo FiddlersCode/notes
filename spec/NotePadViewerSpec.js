fdca9eec3ff4e93e7f4ad76c1e366235f9c9b94e
Robert Jones <robert.g.jones@gmail.com>
2017-05-23 16:43:31 +0100

    Refactor NotePadViewerSpec into separate tests

--- spec/NotePadViewerSpec.js
+++ spec/NotePadViewerSpec.js
@@ -2,11 +2,17 @@
   console.log('Unit test: There is a NotePad Viewer');
   var notepadviewer;
   notepadviewer = new NotePadViewer();
+  assert.isConstructedFrom(notepadviewer, NotePadViewer);
+})();
+
+(function() {
+  console.log('Unit test: NotePadViewer displays outputs notes in HTML');
+  var notepadviewer, notepadmodel;
 
   var notepadmodel = new NotePad();
+  var notepadviewer = new NotePadViewer();
+
   notepadmodel.createNote("hi");
   notepadmodel.createNote("bye");
-
-  assert.isConstructedFrom(notepadviewer, NotePadViewer);
   assert.isEqual('<ul><li><div>hi</div></li></ul>', notepadviewer.displayNotesHTML());
 })();
