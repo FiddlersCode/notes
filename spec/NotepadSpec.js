acf7abcb229d567586b67e132754a4f8522951b2
Robert Jones <robert.g.jones@gmail.com>
2017-05-23 13:20:53 +0100

    Check notepad attribute is populated

--- spec/NotepadSpec.js
+++ spec/NotepadSpec.js
@@ -1,33 +1,30 @@
-// (function() {
-//   console.log('Unit test: NotePad can store and retrieve a note');
-//   var notepad, note;
-//   notepad = new NotePad();
-//   note = new Note('positive');
-//
-//   assert.isConstructedFrom(notepad, NotePad);
-//   notepad.addNote(note);
-//   assert.isConstructedFrom(notepad.getNote(), Note);
-//   assert.isEqual(notepad.getNote().text(),'positive');
-//   console.log('Unit test passed');
-// })();
+(function() {
+  console.log('Unit test: NotePad can store and retrieve a note');
+  var notepad, note;
+  notepad = new NotePad();
+  note = new Note('positive');
+
+  assert.isConstructedFrom(notepad, NotePad);
+  notepad.addNote(note);
+  assert.isConstructedFrom(notepad.getNote(), Note);
+  assert.isEqual(notepad.getNote().text(),'positive');
+  console.log('Unit test passed');
+})();
 
 (function() {
   console.log('Unit test: NotePad can store and retrieve several notes');
   var notepad, note;
   notepad = new NotePad();
-  note1 = new Note('positive');
-  note2 = new Note('very positive');
-  note3 = new Note('ur ok');
-  console.log(note1.text());
-  console.log(note2.text());
-  console.log(note3.text());
-  console.log(notepad);
+  note = new Note('positive');
 
-  notepad.addNote(note1);
-  notepad.addNote(note2);
-  notepad.addNote(note3);
-  console.log(notepad.getAllNotes());
-  assert.isEqual(notepad.length === 3);
-  assert.isEqual(notepad.getAllNotes(),['positive','very positive','ur ok']);
+  var addNotes = function(note, n) {
+    for (var i = 0; i < n; i++) {
+        notepad.addNote(note);
+    }
+  };
+
+  addNotes(note, 5);
+
+  assert.isEqual(notepad.getAllNotes().length, 5);
   console.log('Unit test passed');
 })();
--- src/NotepadModel.js
+++ src/NotepadModel.js
@@ -1,20 +1,15 @@
 function NotePad() {
   this._notes = [];
-  this._allNotes = [];
 }
 
 NotePad.prototype.addNote = function(note) {
   this._notes.push(note);
 };
 
-// NotePad.prototype.getNote = function() {
-//   return this._notes[0];
-// };
+NotePad.prototype.getNote = function() {
+  return this._notes[0];
+};
 
 NotePad.prototype.getAllNotes = function() {
-  this._notes.forEach(function (note) {
-    console.log(note);
-    note.text();
-    this._allNotes.push(note);
-  });
+  return this._notes;
 };
