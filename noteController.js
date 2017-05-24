ad2e598b5b75c00fc23c5c648ea9b27f88af4eb5
SimonTanner <simonthetanner@hotmail.com>
2017-05-24 17:46:56 +0100

    string passed into index.html from NoteController!

--- noteController.js
+++ noteController.js
@@ -2,13 +2,14 @@
   function NoteController(notepad) {
     var notePad = notepad;
     notePad.createNote("test note");
-    this.view = NotePadViewer.displayAll(notePad);
+    var notePadViewer = new NotePadViewer();
+    this.view = notePadViewer.displayAll(notePad);
   }
 
   NoteController.prototype.changeGreeting = function () {
     var elem = document.getElementById("app");
     elem.textContent = this.view;
-  }
+  };
 
   exports.NoteController = NoteController;
 })(this);
